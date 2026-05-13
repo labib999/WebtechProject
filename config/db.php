a135699d24f7dcf8588deb0e931f93afc456145a
labib999 <labibeducation123@gmail.com>
2026-05-14 01:19:29 +0600

    feat(shared): add db config, header, footer, main CSS

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+function getDB() {
+    $host = "sql12.freesqldatabase.com";
+    $db   = "sql12826762";
+    $user = "sql12826762";
+    $pass = "YYrIAFxd6v";
+
+    $conn = new mysqli($host, $user, $pass, $db);
+
+    if ($conn->connect_error) {
+        die("Database connection failed: " . $conn->connect_error);
+    }
+
+    return $conn;
+}
