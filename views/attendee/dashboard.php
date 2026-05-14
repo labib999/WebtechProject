668d8c84be8d4d4d62663e7a7adc496957a35773
marufbytes <marufahammed215@gmail.com>
2026-05-14 14:05:50 +0600

    code to show dashboard

---
+++ views/attendee/dashboard.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="../../public/css/attendee.css">
+</head>
+
+<body>
+
+    <?php include("sidebar.php"); ?>
+
+    <div class="main-content">
+        <h1>Dashboard</h1>
+    </div>
+
+</body>
+</html>
