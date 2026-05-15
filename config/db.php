0cfdf18931825dcba40e7a6d3284256355d0394e
Rohit Roy <rohitroy0396@gmail.com>
2026-05-15 18:24:54 +0600

    feat: add central DB config with freesql credentials

--- config/db.php
+++ config/db.php
@@ -10,6 +10,6 @@ function getDB() {
     if ($conn->connect_error) {
         die("Database connection failed: " . $conn->connect_error);
     }
-
+    $conn->set_charset("utf8mb4");
     return $conn;
 }
